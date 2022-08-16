01343602ad1bdb92d11df3791235fdd02902b162
Igor <romanigor.conect@gmail.com>
2022-08-17 00:25:01 +0200

    add random

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 35;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); // случайное число - 1 2 3 4 ... Calculatorul alege un numar de la 1 la 10 la intamplare
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB ;
 Console.WriteLine(result);
 
