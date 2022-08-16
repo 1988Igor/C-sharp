89b1efc76dced5238f288e21284adc558f8265ee
Igor <romanigor.conect@gmail.com>
2022-08-17 01:05:14 +0200

    if else and ToLower

---
+++ Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter your name: ");
+string username  = Console.ReadLine();
+// ToLower() ne permite ca toate simbolurile din rind sa le treaca in registrul de mai jos//
+if(username.ToLower() == "romica")  // conditia este ca numele trebuie sal indicam cu litere mici
+{
+    Console.WriteLine ("Hello my Friend, Romica ");
+}
+
+else
+{
+    Console.WriteLine("Hello, " + username);
+
+}
