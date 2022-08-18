2748030d97067d810f846ed65bacea706e04dbc1
Igor <romanigor.conect@gmail.com>
2022-08-19 00:33:46 +0200

    dog and friends

---
+++ Example008/Program.cs
@@ -0,0 +1,30 @@
+﻿int count  = 0;
+int distance = 10000;
+int firstFriendSpeed  = 1;
+int secondFriendSpeed = 2;
+int dogspeed = 5;
+int friend = 2;
+int time = 0;
+
+
+
+while (distance > 10)
+{ 
+if (friend == 1)
+{
+
+time = distance/(firstFriendSpeed + dogspeed);
+
+
+}
+else 
+
+
+{
+   time = distance/(firstFriendSpeed + dogspeed);
+   friend  = 2; 
+}
+distance = distance-(firstFriendSpeed+secondFriendSpeed)*time;
+count = count + 1;
+}
+Console.WriteLine (count); 
