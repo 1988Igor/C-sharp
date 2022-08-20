e1b75035bff71bff34d46a76c260c84a8e50c048
Igor <romanigor.conect@gmail.com>
2022-08-20 11:07:52 +0200

    find maximal number from 2 numbers

---
+++ Examplenumber/Program.cs
@@ -0,0 +1,16 @@
+﻿int a,b;
+Console.WriteLine("Enter 2 number:");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+
+    Console.WriteLine("Max number is " + a);
+}
+else
+{
+Console.WriteLine(" Max number is " + b);
+
+}
+Console.ReadLine();
+
