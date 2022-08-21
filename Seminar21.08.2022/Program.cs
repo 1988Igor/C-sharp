49cb1ec715fe63f57cff90ca4f2ca3fe8b5ba53b
Igor <romanigor.conect@gmail.com>
2022-08-21 16:04:04 +0200

    divisible 7 and 23

---
+++ Seminar21.08.2022/Program.cs
@@ -0,0 +1,40 @@
+﻿
+//The code verify if numbers 7 and 23 are divisible wiht another number from Random
+
+bool Divisible7(int x)
+{ 
+bool result = true;
+if ((x%7) == 0)
+{
+    result = true;
+}
+else
+{
+result=false;
+}
+return result;
+ }
+ bool Divisible23(int x)
+ {
+bool result = true;
+if ((x%23) == 0)
+{
+    result  = true;
+}
+else
+{
+result = false;
+}
+return result;
+
+ }
+ Console.WriteLine("Enter a integer number: ");
+ int x = new Random().Next(-100,101);
+ Console.WriteLine(x);
+ if (Divisible7(x))
+ {
+    if (Divisible23(x)) Console.WriteLine($"The number {x} is divisible with 7 and 23");
+    else Console.WriteLine("The number is not divisible");
+ }
+ else Console.WriteLine("The number is not divisible");
+ 
