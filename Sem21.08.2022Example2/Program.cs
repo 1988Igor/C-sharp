b0d4175fdc84020d78dbdcd22668dfcd4e7bbd10
Igor <romanigor.conect@gmail.com>
2022-08-21 16:40:52 +0200

    code with Qadran

---
+++ Sem21.08.2022Example2/Program.cs
@@ -0,0 +1,29 @@
+﻿// This code help us to find in which area is the point into the plane or flat
+Console.WriteLine("Enter the x: ");
+int x = new Random().Next(-5,5);
+Console.WriteLine(x);
+Console.WriteLine("Enter the y: ");
+int y = new Random().Next(-5,5);
+Console.WriteLine(y);
+if(x>0 && y>0)
+{
+    Console.WriteLine("The point ist located in first surface");
+}
+
+else if (x>0 && y>0)
+{
+    Console.WriteLine("The point ist located in second surface");
+}
+else if (x<0 && y<0)
+{
+    Console.WriteLine("The point ist located in third surface");
+}
+else if (x>0 && y<0)
+{
+    Console.WriteLine("The point ist located in thourth surface");
+}
+else
+{
+Console.WriteLine("Enter another value: ");
+
+}
